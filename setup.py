8096679629db17d29f31d403dd1f930eac547dd9
Arturo Oncevay <arturo@Arturos-Macbook-Pro.local>
2025-05-27 13:08:57 +0100

    nltk config error

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 import os
 import sys
 import nltk
-from pathlib import Path
-import json
 
 def setup_nltk_data():
     """Download required NLTK data packages."""
@@ -12,32 +10,13 @@ def setup_nltk_data():
     
     # List of required NLTK data packages
     required_packages = [
-        # For tokenization
         'punkt',  # Sentence tokenizer
         'wordnet',  # For synonym replacement
-        
-        # For entity recognition
         'averaged_perceptron_tagger',  # POS tagger
         'maxent_ne_chunker',  # Named Entity chunker
         'words',  # Words corpus
-        
-        # Additional resources that might be useful
         'omw-1.4',  # Open Multilingual Wordnet
     ]
-    
-    # Create data directory if specified in config
-    config_path = Path('src/config.json')
-    if config_path.exists():
-        with open(config_path, 'r') as f:
-            config = json.load(f)
-            nltk_config = config.get('nltk', {})
-            data_path = nltk_config.get('data_path')
-            
-            if data_path:
-                data_path = Path(data_path)
-                data_path.mkdir(parents=True, exist_ok=True)
-                nltk.data.path.append(str(data_path))
-                print(f"NLTK data will be downloaded to: {data_path}")
     
     # Download each required package
     for package in required_packages:
